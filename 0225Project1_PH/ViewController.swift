0e7e2a5847cdeaf21b123bbf3ae927a4ee8dbcdc
Moonshine  <33474743+moon0411@users.noreply.github.com>
2019-04-03 23:15:50 +0800

    Revert "Add view count dictionary and user defaults"

--- 0225Project1_PH/ViewController.swift
+++ 0225Project1_PH/ViewController.swift
@@ -10,25 +10,17 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+
     var pictures = [String]()
-    
-    // Initialize empty Dictionary to store view count for each picture
-    var viewCount = [String: Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
  
-        title = "Storm View"
+        title = "Strom View"
         navigationController?.navigationBar.prefersLargeTitles = true
         
         performSelector(inBackground: #selector(fetchData), with: nil)
 
-        // Load ViewCount from User Defaults
-        // Check if it exists and if so, cast it as a Dictionary
-        if let viewCount = UserDefaults.standard.object(forKey: "viewCount") as? [String: Int] {
-            // If it exists, overwrite the empty viewCount dictionary
-            self.viewCount = viewCount
-        }
     }
     
     @objc func fetchData() {
@@ -55,15 +47,6 @@ class ViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
-
-        // Check if the key exists, if it does then display the count
-        if let count = viewCount[pictures[indexPath.row]] {
-            cell.detailTextLabel?.text = "Views: \(count)"
-        } else {
-            // Otherwise display this ...
-            cell.detailTextLabel?.text = "Views: None"
-        }
-        
         return cell
     }
     
@@ -74,21 +57,6 @@ class ViewController: UITableViewController {
             vc.current = indexPath.row
             navigationController?.pushViewController(vc, animated: true)
         }
-        
-        // Check if the Key Exists
-        if viewCount[pictures[indexPath.row]] != nil {
-            // Key Exists - Add one to the value
-            viewCount[pictures[indexPath.row]]! += 1
-        } else {
-            // Create the Key - Set the initial value to 1
-            viewCount[pictures[indexPath.row]] = 1
-        }
-        
-        // Save the Dictionary to UserDefaults
-        UserDefaults.standard.set(viewCount, forKey: "viewCount")
-        
-        // Reload the tableView row that was selected
-        tableView.reloadRows(at: [indexPath], with: .automatic)
     }
    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
